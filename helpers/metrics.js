f9cbdf68f9a63bb7ade19f82f5867424f9cff975
Alexis Arias <alexis.arias555@gmail.com>
2022-12-24 00:07:07 -0500

    añadir prometheus

---
+++ helpers/metrics.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const client = require('prom-client')
+require('dotenv').config();
+
+const app = express()
+
+const restResponseTimeHistogram = new client.Histogram({
+    name: "rest_response_time_duration_seconds",
+    help: "REST API response time in seconds",
+    labelNames: ["method", "route", "status_code"]
+})
+
+const databaseResponseTimeHistogram = new client.Histogram({
+    name: "db_response_time_duration_seconds",
+    help: "Database response time in seconds",
+    labelNames: ["operation", "success"],
+});
+
+const startMetricsServer = () => {
+
+    const collectDefaultMetrics = client.collectDefaultMetrics
+
+    collectDefaultMetrics();
+
+    app.get('/metrics', async (req, res) =>{
+        res.set("Content-Type", client.register.contentType)
+        return res.send(await client.register.metrics())
+    })
+
+    app.listen( process.env.PROM_PORT, () => {
+        console.log('Servidor de metricas en el puerto', process.env.PROM_PORT);
+    });
+}
+
+module.exports = {
+    startMetricsServer,
+    restResponseTimeHistogram,
+    databaseResponseTimeHistogram
+}
